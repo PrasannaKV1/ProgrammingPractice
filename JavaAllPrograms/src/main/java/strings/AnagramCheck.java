6e86cde3e530680fa6dd5f9fae3d3ca65beab151
Bipin <bipink@navneettoptech.com>
2025-09-29 17:21:31 +0530

    i have commented out the code

--- JavaAllPrograms/src/main/java/strings/AnagramCheck.java
+++ JavaAllPrograms/src/main/java/strings/AnagramCheck.java
@@ -27,3 +27,27 @@ public class AnagramCheck {
 
     }
 }
+
+//i can also can do it in js
+
+// Function to check if two strings are anagrams
+// function areAnagrams(str1, str2) {
+//     // Remove spaces and convert to lowercase
+//     const normalize = str => str.replace(/\s+/g, '').toLowerCase();
+    
+//     str1 = normalize(str1);
+//     str2 = normalize(str2);
+
+//     // If lengths differ, they cannot be anagrams
+//     if (str1.length !== str2.length) return false;
+
+//     // Sort the characters and compare
+//     const sortedStr1 = str1.split('').sort().join('');
+//     const sortedStr2 = str2.split('').sort().join('');
+
+//     return sortedStr1 === sortedStr2;
+// }
+
+// console.log(areAnagrams("listen", "silent")); // true
+// console.log(areAnagrams("hello", "world"));   // false
+
