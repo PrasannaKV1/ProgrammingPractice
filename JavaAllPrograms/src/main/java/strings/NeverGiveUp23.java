bb219613c95832355b49d5cfd05393ace5b237d0
PrasannaKV1 <prasannapooja215@gmail.com>
2025-12-13 16:50:15 +0530

    this is the commit message

---
+++ JavaAllPrograms/src/main/java/strings/NeverGiveUp23.java
@@ -0,0 +1,22 @@
+package strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NeverGiveUp23 {
+
+    public static void main(String[] args) {
+        String s="prasanna";
+        Map<Character,Integer> map=new HashMap<>();
+        for (char c :s.toCharArray()) {
+            map.put(c,map.getOrDefault(c,0)+1);
+            if(map.get(c)>1)
+            {
+                System.out.println(c);
+            }
+
+            
+        }
+        System.out.println(map);
+    }
+}
